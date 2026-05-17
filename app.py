b1a6ed90110a7905e33e5b93de069919eb980959
Sasa Rakic <rakics@gmail.com>
2026-05-17 17:33:03 +0200

    Pythn - Add syntax verification with >python -m pylint app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""Importing os"""
+import os
 from flask import Flask, request
 from sqlalchemy import create_engine, Column, BigInteger, String, Text, DateTime
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 from dotenv import load_dotenv
-from controllers import AboutUsController, ContactUsController, CvController, TechnologiesController
+from controllers import HomeController, AboutUsController, ContactUsController
+from controllers import CvController, TechnologiesController
 
 # Load the .env file
 load_dotenv()
@@ -33,21 +35,22 @@ engine = create_engine(f"mysql+mysqldb://{db_user}:{db_pass}@{db_host}:{db_port}
 Base = declarative_base()
 
 class ContactUsOrm(Base):
+    """Defining class for sql table contact_us as ORM"""
     __tablename__ = "contact_us"
     id = Column(BigInteger, primary_key = True, autoincrement = True, unique = True)
     client_ip = Column(String(255), nullable = False, default = '')
     first_name = Column(String(255), nullable = False, default = '')
-    last_name = Column(String(255), nullable = False, default = '')    
-    email = Column(String(255), nullable = False, default = '') 
-    subject = Column(String(255), nullable = False, default = '') 
+    last_name = Column(String(255), nullable = False, default = '')
+    email = Column(String(255), nullable = False, default = '')
+    subject = Column(String(255), nullable = False, default = '')
     message = Column(Text)
     created_at = Column(DateTime, default = '0000-00-00 00:00:00')
-    updated_at = Column(DateTime, default = '0000-00-00 00:00:00') 
-    
+    updated_at = Column(DateTime, default = '0000-00-00 00:00:00')
+
 # 3. Create the table in the database
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
+session = sessionmaker(bind=engine)
 
 # from routes import home  # noqa: F401
 
@@ -55,54 +58,61 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 async def index():
-    from controllers import HomeController
+    """Show home html page"""
     home_controller = HomeController
     return home_controller.index()
 
 @app.route("/home/", methods=["GET"])
 async def home():
-    from controllers import HomeController
+    """Show home html page"""
     home_controller = HomeController
     return home_controller.index()
 
 @app.route("/about-us/", methods=["GET"]) # , methods=["GET"]
 async def about_us():
-    about_us_controller = AboutUsController 
-    return about_us_controller.index() 
-    
+    """Show About Us html page"""
+    about_us_controller = AboutUsController
+    return about_us_controller.index()
+
 @app.route("/contact-us/", methods=["GET"])
 def contact_us_index():
+    """Show Contact Us form html page"""
     contact_us_controller = ContactUsController
-    return contact_us_controller.index()      
+    return contact_us_controller.index()
 
 @app.route("/contact-us/", methods=["POST"])
 def contact_us_create():
+    """Create SQL insert data from Contact Us form submit html page"""
     contact_us_controller = ContactUsController
     with engine.connect() as db:
-        html = contact_us_controller.create(Session, ContactUsOrm, request)
+        html = contact_us_controller.create(session, ContactUsOrm, request)
         db.close()
-    return html 
-   
+    return html
+
 @app.route("/contact-us/mail/<string:base64_json>", methods=["GET"])
 def contact_us_mail(base64_json):
+    """show web page with html Email popup after Contact Us form submit"""
     contact_us_controller = ContactUsController
-    return contact_us_controller.mail(Session, ContactUsOrm, base64_json)
-   
+    return contact_us_controller.mail(session, ContactUsOrm, base64_json)
+
 
 @app.route("/contact-us/thank-you/", methods=["GET"])
 def contact_us_thank_you():
-    contact_us_controller = ContactUsController 
+    """show web page with Thank You message after Contact Us form submit"""
+    contact_us_controller = ContactUsController
     return contact_us_controller.thank_you()
 
 @app.route("/cv/", methods=["GET"])
 def cv():
+    """show web page with Cv documents"""
     cv_controller = CvController
     return cv_controller.index()
- 
+
 @app.route("/technologies/", methods=["GET"])
-def technologies(): 
+def technologies():
+    """show web page with Technologies used"""
     technologies_controller = TechnologiesController
-    return technologies_controller.index()   
-    
+    return technologies_controller.index()
+
 if __name__ == '__main__':
     app.run(host='localhost', port=4949)
